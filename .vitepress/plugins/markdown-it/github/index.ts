26ae284201cce277603c3c7a64d9c9a6e4fd5de7
ubugeeei <71201308+ubugeeei@users.noreply.github.com>
2024-09-25 22:08:28 +0900

    fix: hydration mismatch (#71)

--- .vitepress/plugins/markdown-it/github/index.ts
+++ .vitepress/plugins/markdown-it/github/index.ts
@@ -6,14 +6,27 @@ import { renderToHtml } from "./render";
 import { fetchHtmlSync } from "./fetch";
 
 export const previewGitHubSource: PluginSimple = (md) => {
-  md.renderer.rules.text = (tokens, idx) => {
-    let c = tokens[idx].content;
-    if (isGithubUrl(c)) {
-      const rawHtml = fetchHtmlSync(c);
-      const parsed = parse(rawHtml, c);
-      const res = renderToHtml(parsed);
-      return res;
+  md.block.ruler.before('paragraph', 'github_link_block', (state, startLine) => {
+    const line = state.getLines(startLine, startLine + 1, state.blkIndent, false).trim()
+
+    if (!isGithubUrl(line)) {
+      return false
     }
-    return c;
-  };
-};
+
+    const token = state.push('github_link', '', 0)
+    token.content = line
+    token.map = [startLine, startLine + 1]
+
+    state.line = startLine + 1
+
+    return true
+  })
+
+  md.renderer.rules.github_link = (tokens, idx) => {
+    const c = tokens[idx].content
+    const rawHtml = fetchHtmlSync(c)
+    const parsed = parse(rawHtml, c)
+    const res = renderToHtml(parsed)
+    return res
+  }
+}
