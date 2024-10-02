2c78ff8376929fd1373bb9dc506fc16fd1eb9669
babu-ch <42715882+babu-ch@users.noreply.github.com>
2024-10-02 13:03:44 +0900

    feat: display line number (#90)
    * feat: display line number
    
    feat: display line number
    
    * feat: add commit link

--- .vitepress/plugins/markdown-it/github/render.ts
+++ .vitepress/plugins/markdown-it/github/render.ts
@@ -13,6 +13,8 @@ export function renderToHtml(descriptor: GitHubSourceDescriptor): string {
   const { filename, lang, commitHashOrBranch, lines, codeLines } = descriptor;
   const [org, repo, ...rest] = filename.split("/");
 
+  const commitLink = `https://github.com/${org}/${repo}/commit/${commitHashOrBranch}`
+
   let codeLink = `https://github.com/${org}/${repo}/blob/${commitHashOrBranch}/${rest.join(
     "/"
   )}`;
@@ -29,18 +31,21 @@ export function renderToHtml(descriptor: GitHubSourceDescriptor): string {
 
   const code = hljs.highlight(c, {
     language: lang,
-  }).value;
+  }).value.split("\n").map((line, i) => {
+    return `<span class="line-number">${i + 1 + (lines?.start ?? 0)}</span>${line}`
+  });
 
   return `
 <div class="github-source">
   <div class="meta">
-    <img src="https://cdn0.iconfinder.com/data/icons/shift-logotypes/32/Github-512.png" width="14px" style="display: inline; margin-right: 0.5rem; vertical-align: middle;">
-    <a href="${codeLink}" target="_blank"
-      ><span class="filename">${filename}</span></a
-    > <span class="at">at</span> <a href="${codeLink}" target="_blank"
-      ><span class="commit">${commitHashOrBranch.slice(0, 10)}</span>
-    </a>
+    <img src="https://cdn0.iconfinder.com/data/icons/shift-logotypes/32/Github-512.png" width="25px" style="display: inline; margin-right: 0.5rem; vertical-align: middle;">
+    <span class="file-info">
+      <a href="${codeLink}" target="_blank"
+        ><span class="filename">${filename}</span></a
+      ><span class="line-info"
+        >Lines ${lines?.start ? lines.start + 1 : 1} to ${lines?.end ?? code.length} in <a href="${commitLink}" target="_blank"><span class="commit">${commitHashOrBranch.slice(0, 10)}</span></a></span>
+    </span>
   </div>
-  <pre class="code">${code}</pre>
+  <pre class="code">${code.join("\n")}</pre>
 </div>`;
 }
